5a8668d41f587a1ae0b6cc71cf2c69f56f54f4f1
MrCosik <56444584+MrCosik@users.noreply.github.com>
2021-06-18 22:50:45 +0200

    Initial commit

---
+++ src/main/java/pl/pro/PasswordApp/PasswordAppApplication.java
@@ -0,0 +1,13 @@
+package pl.pro.PasswordApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pro/PasswordApp/PasswordAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pro.PasswordApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
